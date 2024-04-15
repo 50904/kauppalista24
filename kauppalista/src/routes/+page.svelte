cb6691a970d880c6ceba1df28e80a79f58be877c
Joonatan Paijo <156064167+50904@users.noreply.github.com>
2024-04-15 13:45:19 +0300

    html,css

--- kauppalista/src/routes/+page.svelte
+++ kauppalista/src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let asiat = ['Leipä', 'Tomaatti', 'Kurkku'];
+
+    let uusiAsia = '';
+
+    function lisaaAsia(event) {
+        event.preventDefault();
+        asiat.push(uusiAsia);
+        asiat = asiat;
+        uusiAsia = '';
+    }
+</script>
+
+<div class="komponentti">
+    <h1>Kauppalista</h1>
+    <ul>
+      {#each asiat as asia}
+        <li>
+           <input type="checkbox" />
+           {asia}
+        </li>
+      {/each}
+    <ul>
+    <form class="uusi">
+        <label for= "uusi-asia">Lisää uusi:</label>
+        <input id="uusi-asia" type="text" bind:value={uusiAsia} />
+        <button on:click|preventDefault={lisaaAsia}>Lisää</button>
+    </form>
+</div>
+<style>
+     .komponentti {
+        color: hwb(247 12% 74%);
+        font-family: 'Segoe UI' , Tahoma, Geneva , Verdana, Sans-serif;
+        font-size: 12px;
+    }
+    h1 {
+        font-size: 200%
+    }
+    ul {
+        font-size: 150%;
+    }
+
+    li {
+        list-style-type: none;
+    }
+    .uusi {
+        font-size: 125%;
+    }
+</style>
+
